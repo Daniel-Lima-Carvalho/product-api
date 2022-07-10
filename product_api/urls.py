b8f7843ca85c3490859b59bf9502948906c7afed
Daniel <daniellima2297@gmail.com>
2022-07-10 20:03:56 -0300

    Installing django rest api

--- product_api/urls.py
+++ product_api/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='/admin', permanent=False)),
+    path('api-auth/', include('rest_framework.urls'))
 ]
